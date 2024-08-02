4fb6d71e4a06ae7b73274f005dacc1f7702cc1ef
Sojus85 <dj-andrex@mail.ru>
2024-08-02 14:39:34 +0300

    firebase relese

--- module.js
+++ module.js
@@ -1,36 +1,44 @@
-import firebase from 'firebase/app';
-import  'firebase/database';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4e5O87qFqXO7-0W7V3qfXO7-0W7V3qfX",
-  authDomain: "gamelist-sojus85.firebaseapp.com",
-  databaseURL: "https://gamelist-sojus85.firebaseio.com",
-  projectId: "gamelist-sojus85",
-  storageBucket: "gamelist-sojus85.appspot.com",
-  messagingSenderId: "891056867322",
-  appId: "1:891056867322:web:abcdefghijklmnopqrstuvwxyz"
-};
+    apiKey: "AIzaSyDucoVbrpJnWu_pYMqcNqw5caCBwxoK45s",
+    authDomain: "gamelist-sojus85.firebaseapp.com",
+    databaseURL: "https://gamelist-sojus85-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gamelist-sojus85",
+    storageBucket: "gamelist-sojus85.appspot.com",
+    messagingSenderId: "891056867322",
+    appId: "1:891056867322:web:36d52855cc440a05648643"
+  };
+  
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
-firebase.initializeApp(firebaseConfig);
- 
 function submitForm(event) {
-    event.preventDefault();
-  
-    // Получает данные формы
-    const gameName = document.getElementById('game-name').value;
-    const gameType = document.querySelector('input[name="game-type"]:checked').value;
-    const gameLink = document.getElementById('game-link').value;
-    const gameDescription = document.getElementById('game-description').value;
-  
-    // Создает объект с данными формы
-    const formData = {
-      gameName,
-      gameType,
-      gameLink,
-      gameDescription
-    };
-  
-    // Записывает данные формы в базу данных Firebase Realtime Database
-    firebase.database().ref('games').push(formData);
-  }
-  
+  event.preventDefault();
+
+  // Получаем данные формы
+  const gameName = document.getElementById('game-name').value;
+  const gameType = document.querySelector('input[name="game-type"]:checked').value;
+  const gameLink = document.getElementById('game-link').value;
+  const gameDescription = document.getElementById('game-description').value;
+
+  // Создаем объект с данными формы
+  const formData = {
+    gameName,
+    gameType,
+    gameLink,
+    gameDescription
+  };
+
+  // Записываем данные формы в базу данных Firebase Realtime Database
+  push(ref(db, 'games'), formData).then(() => {
+    console.log('Игра добавлена!');
+    document.getElementById('add-game-form').reset();
+  }).catch((error) => {
+    console.error('Ошибка добавления игры: ', error);
+  });
+}
+
+document.getElementById('add-game-form').addEventListener('submit', submitForm);
--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4e5O87qFqXO7-0W7V3qfXO7-0W7V3qfX",
+  authDomain: "gamelist-sojus85.firebaseapp.com",
+  databaseURL: "https://gamelist-sojus85.firebaseio.com",
+  projectId: "gamelist-sojus85",
+  storageBucket: "gamelist-sojus85.appspot.com",
+  messagingSenderId: "891056867322",
+  appId: "1:891056867322:web:abcdefghijklmnopqrstuvwxyz"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Функция для отображения игр
+function fetchGames() {
+  const gamesList = document.getElementById('games-list');
+  gamesList.innerHTML = '';
+
+  onValue(ref(db, 'games'), (snapshot) => {
+    snapshot.forEach((childSnapshot) => {
+      const game = childSnapshot.val();
+      const li = document.createElement('li');
+      li.textContent = `${game.gameName} - ${game.gameType} - ${game.gameLink} - ${game.gameDescription}`;
+      gamesList.appendChild(li);
+    });
+  });
+}
+
+// Проверка, на какой странице мы находимся, и выполнение соответствующих функций
+if (window.location.pathname.endsWith('/index.html')) {
+  fetchGames();
+}
 
 // Добавление кнопки "Вернуться к списку игр" на всех страницах, кроме главной страницы
 if (!window.location.pathname.endsWith('/index.html')) {
@@ -7,4 +42,3 @@ if (!window.location.pathname.endsWith('/index.html')) {
   backButton.classList.add('back-button');
   document.body.appendChild(backButton);
 }
-
