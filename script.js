0035da011423aeb1c6c42f8e6c06c31a224fc5d4
Sojus85 <33547906+Sojus85@users.noreply.github.com>
2024-08-04 21:03:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Добавление кнопки "Вернуться к списку игр" на всех страницах, кроме главной страницы
 if (!window.location.pathname.endsWith('/index.html')) {
   const backButton = document.createElement('a');
-  backButton.href = '/';  // Или используйте '/index.html' если это подходит
+  backButton.href = 'game-list';  // Или используйте '/index.html' если это подходит
   backButton.textContent = 'Вернуться к списку игр';
   backButton.classList.add('back-button');
   document.body.appendChild(backButton);
